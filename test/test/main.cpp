ab2039e7b83a044b64aec868ebb00eece2c1a005
Baibing <bl285@uowmail.edu.au>
2017-04-06 05:49:27 +1000

    test push code

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by bl285 on 6/4/17.
+//  Copyright © 2017 bl285. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
